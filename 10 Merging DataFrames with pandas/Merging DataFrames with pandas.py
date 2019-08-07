28bd90d0154acd87f5df8196507e917e077f532f
Amin Yakubu <amin@genesisrg.com>
2019-08-06 22:21:43 -0400

    10 merging DF

--- 10 Merging DataFrames with pandas/Merging DataFrames with pandas.py
+++ 10 Merging DataFrames with pandas/Merging DataFrames with pandas.py
@@ -40,12 +40,30 @@ for filename in filenames:
 # Print top 5 rows of 1st DataFrame in dataframes
 print(dataframes[0].head())
 
+# =============================================================================
+# Using comprehensions
+# =============================================================================
 
-# Combining DataFrames from multiple data files
-#In this exercise, you'll combine the three DataFrames from earlier exercises - 
-#gold, silver, & bronze - into a single DataFrame called medals. The approach 
-#you'll use here is clumsy. Later on in the course, you'll see various powerful 
-#methods that are frequently used in practice for concatenating or merging DataFrames.
+filenames = ['data/sales-jan-2015.csv', 'data/sales-feb-2015.csv']
+
+dataframes = [pd.read_csv(f) for f in filenames] 
+
+# =============================================================================
+# Using glob
+# =============================================================================
+
+from glob import glob
+filenames = glob('sales*.csv')
+dataframes = [pd.read_csv(f) for f in filenames]
+
+# =============================================================================
+# # Combining DataFrames from multiple data files
+# 
+# #In this exercise, you'll combine the three DataFrames from earlier exercises - 
+# #gold, silver, & bronze - into a single DataFrame called medals. The approach 
+# #you'll use here is clumsy. Later on in the course, you'll see various powerful 
+# #methods that are frequently used in practice for concatenating or merging DataFrames.
+# =============================================================================
 
 # Import pandas
 import pandas as pd
@@ -65,3 +83,57 @@ medals['Bronze'] = bronze['Total']
 
 # Print the head of medals
 print(medals.head())
+
+# =============================================================================
+# Reindexing
+# DataFrames
+# =============================================================================
+# =============================================================================
+# Sorting DataFrame with the Index & columns
+# =============================================================================
+
+#● indices: many index labels within Index data structures
+#● indexes: many pandas Index data structures
+
+w_mean = pd.read_csv('data/quarterly_mean_temp.csv', index_col='Month')
+w_max = pd.read_csv('data/quarterly_max_temp.csv', index_col='Month')
+
+print(w_mean.index)
+print(w_max.index)
+
+#Using .reindex
+
+ordered = ['Jan', 'Apr', 'Jul', 'Oct']
+w_mean2 = w_mean.reindex(ordered)
+print(w_mean2)
+
+# Using .sort_index()
+
+w_mean2.sort_index()
+
+# Reindex from a DataFrame Index
+
+w_mean.reindex(w_max.index)
+
+# Reindexing with missing labels
+
+w_mean3 = w_mean.reindex(['Jan', 'Apr', 'Dec']) 
+print(w_mean3)
+
+w_max.reindex(w_mean3.index).dropna()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
