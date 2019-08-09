caa413a02a37c466c2feafd73d6a57e240e91a0a
Amin Yakubu <amin@genesisrg.com>
2019-08-09 08:54:41 -0400

    regression

--- 21 Supervised Learning with scikit-learn/21 supervised learning with Scikit-learn.py
+++ 21 Supervised Learning with scikit-learn/21 supervised learning with Scikit-learn.py
@@ -162,6 +162,175 @@ new_prediction = knn.predict(X_new)
 print("Prediction: {}".format(new_prediction))
 
 
+# =============================================================================
+
+# =============================================================================
+# Measuring model performance
+# =============================================================================
+#Train/test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, 
+                                                    random_state=21, stratify=y)
+
+knn = KNeighborsClassifier(n_neighbors=8)
+
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n {}".format(y_pred))
+
+knn.score(X_test, y_test) 
+
+#Model complexity
+#● Larger k = smoother decision boundary = less complex model
+#● Smaller k = more complex model = can lead to overfitting
+
+# =============================================================================
+# The digits recognition dataset
+# =============================================================================
+
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits.DESCR)
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+#Train/Test Split + Fit/Predict/Accuracy
+#
+#Now that you have learned about the importance of splitting your data into 
+#training and test sets, it's time to practice doing this on the digits dataset! 
+#
+#After creating arrays for the features and target variable, you will split them
+#into training and test sets, fit a k-NN classifier to the training data, and 
+#then compute its accuracy using the .score() method.
+
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# Create feature and target arrays
+X = digits.data
+y = digits.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify= y)
+
+#Stratify the split according to the labels so that they are distributed in the 
+#training and test sets as they are in the original dataset.
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors = 7)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+# =============================================================================
+# Overfitting and underfitting
+# =============================================================================
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train, y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+
+
+#It looks like the test accuracy is highest when using 3 and 5 neighbors. 
+#Using 8 neighbors or more seems to result in a simple model that underfits the data. 
+
+# =============================================================================
+# Introduction to regression
+# =============================================================================
+
+# Boston housing data
+
+boston = pd.read_csv('data/boston-housing/train.csv')
+
+print(boston.head()) 
+
+
+# Creating feature and target arrays
+
+X = boston.drop('medv', axis=1).values
+y = boston['medv'].values
+
+# Predicting house value from a single feature
+
+X_rooms = X[:,6]
+type(X_rooms), type(y)
+
+y = y.reshape(-1, 1)
+
+X_rooms = X_rooms.reshape(-1, 1)
+
+plt.scatter(X_rooms, y)
+plt.ylabel('Value of house /1000 ($)')
+plt.xlabel('Number of rooms')
+plt.show();
+
+# Fitting a regression model
+
+import numpy as np
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(X_rooms, y)
+prediction_space = np.linspace(min(X_rooms), 
+                               max(X_rooms)).reshape(-1, 1)
+
+plt.scatter(X_rooms, y, color='blue')
+plt.plot(prediction_space, reg.predict(prediction_space),
+         color='black', linewidth=3)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
